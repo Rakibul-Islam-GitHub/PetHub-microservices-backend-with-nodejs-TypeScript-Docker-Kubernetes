759dec4f33bbb0d3a129ebb3349054b92fb1276c
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-02-11 02:12:18 +0600

    current user router added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,16 @@
 import express, { json } from "express";
-
+import {currentUserRouter} from "./routes/current-user";
 const app = express()
 app.use(json())
+
+app.use(currentUserRouter);
+
 
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
+
 app.listen(3000, ()=>{
-    console.log("App is Running on port 3000")
+    console.log("App is Running on port- 3000!")
 })
