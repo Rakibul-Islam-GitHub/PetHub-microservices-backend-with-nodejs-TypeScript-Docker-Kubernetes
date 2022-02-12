0b7c48f77e2329889ff4ac26098b1618529a2eb7
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-02-12 22:32:25 +0600

    signup route added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express, { json } from "express";
 import {currentUserRouter} from "./routes/current-user";
+import { signupRouter } from "./routes/signup";
 const app = express()
 app.use(json())
 
 app.use(currentUserRouter);
+app.use(signupRouter);
 
 
 app.get('/', (req, res) => {
