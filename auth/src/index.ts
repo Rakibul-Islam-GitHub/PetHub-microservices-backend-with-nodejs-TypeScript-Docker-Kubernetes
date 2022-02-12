9fc757db74d21cb5fb39912b7e60778d721d7c25
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-02-12 22:37:00 +0600

    signout route added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import {currentUserRouter} from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 const app = express()
 app.use(json())
@@ -8,6 +9,7 @@ app.use(json())
 app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
+app.use(signoutRouter);
 
 
 app.get('/', (req, res) => {
