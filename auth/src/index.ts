c3d645833d3988cd97169bafc8b7bbf346144b1d
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-02-12 22:36:06 +0600

    signin route added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,13 @@
 import express, { json } from "express";
 import {currentUserRouter} from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 const app = express()
 app.use(json())
 
 app.use(currentUserRouter);
 app.use(signupRouter);
+app.use(signinRouter);
 
 
 app.get('/', (req, res) => {
