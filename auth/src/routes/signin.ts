41d753cafcaa2d404ff8b482f2986e2340fd3b66
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-02-15 06:29:28 +0600

    added some validation rule into signin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult  } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-    res.send("working...")
-});
+router.post(
+  "/api/users/signin",
+    [
+        body("email").isEmail().withMessage("Please enter a valid email address"),
+        body("password").trim().isLength({ min: 3, max: 20 }).withMessage('Please enter a password between 3 to 20 characters')
+    ],
+    (req: Request, res: Response) => {
+      const errors= validationResult(req);
+      if (!errors.isEmpty()) {
+
+        res.status(400).send(errors.array());
+          
+      }
+    res.send({});
+  }
+);
 
 export { router as signinRouter };
