14155c104f69867c5ba67273f148e400746a73f1
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-02-17 08:16:33 +0600

    signup updated

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/requestValidationError";
+
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
+router.post("/api/users/signup", [
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('password').isLength({min:3, max:15}).withMessage('Password should be between 3 to 15 characters')
+], (req: Request, res: Response) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        
+        throw new RequestValidationError(errors.array());
+        
+    }
+
     res.send("working...")
 });
 
